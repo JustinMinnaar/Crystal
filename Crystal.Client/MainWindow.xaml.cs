fda99352c7fd2135b37ea4d30d7211845c5ab74b
Justin Minnaar <justin@minnaar.org>
2017-07-15 16:52:21 +0200

    Added a DATA project to the solution, with an example simple list of types.

--- Crystal.Client/MainWindow.xaml.cs
+++ Crystal.Client/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Crystal.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,13 @@ namespace Crystal.Client
         public MainWindow()
         {
             InitializeComponent();
+            InitialiseListOfStuff();
+        }
+
+        private void InitialiseListOfStuff()
+        {
+            var stuff = new CrystalStuff();
+            lstTypes.ItemsSource = stuff.Types;
         }
     }
 }
---
+++ Crystal.Data/CrystalStuff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Crystal.Data
+{
+    public class CrystalStuff
+    {
+        public CrystalStuff()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Types.Add("TYPE #" + i);
+            }
+
+        }
+
+        public List<string> Types = new List<string>();
+    }
+}
